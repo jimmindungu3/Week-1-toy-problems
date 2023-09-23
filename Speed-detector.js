6bd67c3557841a10602dea73862f690f67a67352
jamesndungu <jimmindungu3@gmail.com>
2023-09-23 14:04:02 +0300

    Solve quiz 2

---
+++ Speed-detector.js
@@ -0,0 +1,24 @@
+
+
+// This program will take the speed of a car as input and give the driver an "OK" message if within speed limit. Else, the driver will get demerit points up to 12. 13 points or more equal licence suspension.
+
+function speedDetector(driverSpeed) {
+    const speedLimit = 70;
+    // Check if the driver speed is below the speed limit and print OK message.
+    if (driverSpeed < speedLimit) {
+        console.log("OK");
+        return;     // Terminate function here if within speedLimit.
+    }
+
+    const demeritPoints = Math.floor((driverSpeed - 70) / 5); // This line calculates demerit points and assigns the result to a variable: demeritPoints. The Math.floor method is used to round down the result to a whole number.
+
+    switch (true) { // Switch statement checks if demeritPoints are below 12. If true, demerit points will be printed, else licence is suspended.
+        case demeritPoints <= 12:
+            console.log("Points: " + demeritPoints);
+            break;
+    default:
+        console.log("Licence suspended");
+    }        
+}
+
+speedDetector(95); // Invokes the speedDetector function and passes driver speed as argument. 135 in this case.
--- Student-grade-generator.js
+++ Student-grade-generator.js
@@ -1,1 +1,22 @@
-// 
+
+// This function takes student marks as input and returns the student grade based on a grading system
+// @param {number} studentScore - the student's score. A valid score should range from 0 to 100.
+// @return {string} - returns the grade the student has scored as per the grading system. "A", "B", "C", "D" OR "E"
+
+
+function studentGradeGenerator(studentScore) {
+    if (studentScore < 0 || studentScore > 100) {       // This line of code checks if studentScore is a valid score.
+        return "Invalid marks! Marks should range from 0 to 100"
+    } else if (studentScore < 40) {
+        studentGrade = "E";
+    } else if (studentScore <= 49) {
+        studentGrade = "D";
+    } else if (studentScore <= 59) {
+        studentGrade = "C";
+    } else if (studentScore <= 79) {
+        studentGrade = "B";
+    } else
+        studentGrade = "A";
+    // console.log(studentGrade); // This line is commented out. It will print studentGrade to the console when uncommented.
+}
+studentGradeGenerator(); // This line invokes the function studentGradeGenerator. StudentScore is passed as an argument in the brackets.
